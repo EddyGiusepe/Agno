5bae7f24e902fb0faa4af81f0ccfd5b05c8539eb
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-04-05 00:22:18 -0300

    settings

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+import os
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
